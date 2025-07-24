2d809c56e897f47884af28b0301770fdac92e75e
Pranav Jayakumar <167646591+pranaaavj@users.noreply.github.com>
2025-07-25 00:28:00 +0530

    Time: 0 ms (100%), Space: 54.4 MB (64.83%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in map) {
+      return [map[target - nums[i]], i]
+    } else { 
+      map[nums[i]] = i
+    }
+  }
+};

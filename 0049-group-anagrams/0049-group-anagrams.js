1bfc9293e05a6bbe65e677691f75f0e41a3682cd
Pranav Jayakumar <167646591+pranaaavj@users.noreply.github.com>
2025-07-29 09:47:14 +0530

    Time: 39 ms (35.08%), Space: 69.4 MB (19.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,13 +3,6 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  // Brute force
-  // Split each word into array using map 
-  // Compare one word (array) to the rest of the words if they match ( use includes )
-  // If they do push into one array 
-  // Else just keep that word we used to compare in a array and push that
-  // This is not at all optimised, but we can figure out more efficient way 
-
   let grouped = {}
 
   for (const s of strs) {

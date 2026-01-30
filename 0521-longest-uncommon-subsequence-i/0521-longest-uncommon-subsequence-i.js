bd0d15943b3b04bb3ab1ff6fe615961ba20b23fe
Pranav Jayakumar <167646591+pranaaavj@users.noreply.github.com>
2026-01-30 13:16:11 +0530

    Time: 0 ms (100%), Space: 54.6 MB (14.29%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function(a, b) {
+    if (a === b) return -1
+    else if (a.length > b.length) return a.length
+    else return b.length
+};
